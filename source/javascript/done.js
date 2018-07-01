c9026faa3113f12888898a76cf2db2b3c31fcd62
RyougiNevermore <ryougi.nevermore@hotmail.com>
2018-07-02 05:54:21 +0800

    UPDATE DONE.JS

--- source/javascript/done.js
+++ source/javascript/done.js
@@ -18,3 +18,31 @@ function showDialog(_id) {
 function jump(_url) {
     window.location.href = _url;
 }
+
+// search boot
+function search_boot() {
+    var _text = $('#search_input').val();
+    if (_text === undefined || _text.trim() === '') {
+        $('#posts_chips').children().show();
+        return false;
+    }
+    _text = _text.trim();
+    $('#posts_chips').children().each(function (_, chip) {
+        var _post_title = $(chip).text();
+        if (_post_title !== undefined || _post_title !== '') {
+            if (_post_title.toLowerCase().trim().indexOf(_text.toLowerCase()) > 0) {
+                console.log(_post_title);
+                $(chip).show();
+            } else {
+                $(chip).hide();
+            }
+        }
+    });
+}
+
+$(document).ready(function() {
+    $('figure').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
+});
+
